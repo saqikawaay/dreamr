4e227acda724ab9b49e749973ae0d76cd3272a48
Trevor Anderson <saqikawaay@gmail.com>
2023-12-24 14:31:19 +1000

    mostly finished

--- server.js
+++ server.js
@@ -1,30 +1,41 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
-import { Configuration, OpenAIApi } from 'openai';
 
-const configuration = new Configuration({
+import OpenAI from 'openai';
+
+
+const openai = new OpenAI({
     apiKey: process.env.OPENAI,
 });
-
-const openai = new OpenAIApi(configuration);
-
 import express from 'express';
-
 import cors from 'cors';
+
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/dream', async (req, res) => {
-    const prompt = req.body.prompt;
 
-    const aiResponse = await openai.createImage({
-        prompt, 
-        n: 1,
-        size: '1024x1024',
-    });
-    const image = aiResponse.data[0].url;
-    res.send({ image });
+app.post('/dream', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+
+        const aiResponse = await openai.images.generate({
+            prompt, 
+            n: 1,
+            size: '1024x1024',
+        });
+        const image = aiResponse.data[0].url;
+        res.send({ image });
+    } catch (error) {
+        console.error(error);
+        if (error.response && error.response.data && error.response.data.error) {
+            res.status(500).send(error.response.data.error.message || 'something went wrong');
+        } else {
+            res.status(500).send('something went wrong');
+        }
+    }
 });
 
 
